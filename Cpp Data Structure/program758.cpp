71f0a037cb61e4e05a812c6274d3fec996fc7b7c
Anuja Santosh Dhanwate <dhanwateanuja@gmail.com>
2026-02-19 23:31:46 +0530

    Add files via upload

---
+++ Cpp Data Structure/program758.cpp
@@ -0,0 +1,252 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+         int *Arr;
+         int iSize;
+         bool sorted;
+
+
+   ArrayX(int no);
+   ~ArrayX();
+   void Accept();
+   void Display();
+   bool LinearSearch(int iNo);
+   bool BiDirectionalSearch(int iNo);
+   bool CheckSorted();
+   bool BinarySearch(int iNo);
+
+
+
+
+};
+
+ArrayX :: ArrayX(int no)
+{
+   cout<<"Inside constructor\n";
+   iSize = no;
+   Arr = new int[iSize];
+   sorted = true;
+}
+
+ArrayX :: ~ArrayX()
+{
+   cout<<"Inside destructor\n";
+   delete [] Arr;
+}
+
+void ArrayX :: Accept()
+{
+   int iCnt = 0;
+   cout<<"Inter the elements : \n";
+
+   cin>>Arr[iCnt];
+   for(iCnt = 1 ; iCnt < iSize; iCnt++)
+   {
+      cin>>Arr[iCnt];
+      if(Arr[iCnt -1] > Arr[iCnt])
+      {
+         sorted = false;
+
+      }
+   }
+
+   //sorted = CheckSorted();
+
+
+}
+
+void ArrayX:: Display()
+{
+   int iCnt = 0;
+   cout<<"Elements of the array are : \n";
+   for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+   {
+      cout<<Arr[iCnt]<<"\t";
+   }
+   cout<<"\n";
+
+
+}
+
+bool ArrayX:: LinearSearch(int iNo)
+{
+   bool bFlag = false;
+   int i = 0;
+
+   for(i = 0 ; i< iSize; i++)
+   {
+      if(Arr[i] == iNo)
+      {
+         bFlag = true;
+         break;
+
+      }
+
+   }
+
+   return bFlag;
+
+
+}
+
+
+
+bool  ArrayX::BiDirectionalSearch(int iNo)
+{
+   bool bFlag = false;
+   int iStart = 0;
+   int iEnd = 0;
+
+
+
+   for(iStart = 0 , iEnd = iSize -1; iStart <= iEnd ; iStart++ , iEnd--)
+   {
+      if(Arr[iStart] == iNo || Arr[iEnd] == iNo)
+      {
+         bFlag = true;
+         break;
+
+      }
+
+   }
+
+   return bFlag;
+
+
+}
+
+// 0  1  2  3  4  5  6   7  8  9
+// 12 14 17 24 28 36 48 52 52 65
+bool ArrayX:: CheckSorted()
+{
+   int i = 0;
+   bool bFlag = true;
+
+   for(i = 0 ; i < iSize-1 ; i++)
+   {
+        if(Arr[i] > Arr[i+1])
+        {
+            bFlag = false;
+            break;
+        }
+
+   }
+
+   return bFlag;
+
+
+}
+
+
+bool ArrayX :: BinarySearch(int iNo)
+{
+   int iStart = 0 , iEnd = 0 , iMid = 0;
+   bool bFlag = false;
+
+   if(sorted == false)
+   {
+      return BiDirectionalSearch(iNo);
+      
+   }
+
+   iStart = 0;
+   iEnd = iSize -1;
+
+   while(iStart <= iEnd)
+   {
+      iMid = iStart + ((iEnd - iStart) /2);
+
+      if(Arr[iMid] == iNo)
+      {
+         bFlag = true;
+         break;
+      }
+      else if(iNo < Arr[iMid])
+      {
+         iEnd = iMid -1;
+
+
+
+      }
+      else if(iNo > Arr[iMid])
+      {
+         iStart = iMid +1;
+
+
+      }
+
+   }
+
+   return bFlag;
+}
+
+
+
+
+
+int main()
+{
+    
+    int iValue = 0;
+    
+    cout<<"Enter the number of elements : \n";
+    cin>>iValue;
+
+    ArrayX aobj(iValue);
+
+    aobj.Accept();
+    aobj.Display();
+/*
+     if(aobj.LinearSearch(21))
+     {
+        cout<<"Element is present\n";
+
+     }
+     else
+     {
+        cout<<"There is no such element\n";
+     }
+
+     if(aobj.BiDirectionalSearch(21))
+     {
+        cout<<"Element is present\n";
+
+     }
+     else
+     {
+        cout<<"There is no such element\n";
+     }
+
+
+     if(aobj.sorted == true)
+     {
+        cout<<"data is sorted\n";
+     }
+     else
+     {
+        cout<<"Data is not sorted\n";
+     }
+
+*/
+
+    if(aobj.BinarySearch(25))
+    {
+        cout<<"Element is present \n";
+    }
+    else
+    {
+       cout<<"there is no such element \n";
+
+    }
+
+
+
+    
+
+
+    
+    return 0;
+}
---
+++ Cpp Data Structure/program762.cpp
@@ -0,0 +1,168 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+         int *Arr;
+         int iSize;
+         bool sorted;
+
+
+   ArrayX(int no);
+   ~ArrayX();
+
+
+   void Accept();
+   void Display();
+   
+   bool CheckSorted();
+
+   void BubbleSort();
+  
+
+
+
+
+};
+
+ArrayX :: ArrayX(int no)
+{
+   cout<<"Inside constructor\n";
+   iSize = no;
+   Arr = new int[iSize];
+   sorted = true;
+}
+
+ArrayX :: ~ArrayX()
+{
+   cout<<"Inside destructor\n";
+   delete [] Arr;
+}
+
+void ArrayX :: Accept()
+{
+   int iCnt = 0;
+   cout<<"Inter the elements : \n";
+
+   cin>>Arr[iCnt];
+   for(iCnt = 1 ; iCnt < iSize; iCnt++)
+   {
+      cin>>Arr[iCnt];
+      if(Arr[iCnt -1] > Arr[iCnt])
+      {
+         sorted = false;
+
+      }
+   }
+
+   //sorted = CheckSorted();
+
+
+}
+
+void ArrayX:: Display()
+{
+   int iCnt = 0;
+   cout<<"Elements of the array are : \n";
+   for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+   {
+      cout<<Arr[iCnt]<<"\t";
+   }
+   cout<<"\n";
+
+
+}
+
+// 0  1  2  3  4  5  6   7  8  9
+// 0  1  2  3  4  5  6   7  8  9
+// 12 14 17 24 28 36 48 52 52 65
+bool ArrayX:: CheckSorted()
+{
+   int i = 0;
+   bool bFlag = true;
+
+   for(i = 0 ; i < iSize-1 ; i++)
+   {
+        if(Arr[i] > Arr[i+1])
+        {
+            bFlag = false;
+            break;
+        }
+
+   }
+
+   return bFlag;
+
+
+}
+
+
+// 21 15 18 16 11
+void ArrayX :: BubbleSort()
+{
+   int i = 0 , j = 0;
+   int temp = 0;
+   if(sorted == true)    // important filter
+   {
+      return;
+   }
+
+   for(i = 0; i <iSize -1 ; i++)    // Outer loop
+   {
+
+
+      for(j = 0 ; j< iSize-1-i ; j++)   // inner
+      {
+         if(Arr[j] > Arr[j+1])
+         {
+            temp = Arr[j];
+            Arr[j] = Arr[j+1];
+            Arr[j+1] = temp;
+
+         }
+          
+
+      }
+
+      cout<<"\nData after pass : "<<i+1<<"\n";
+      Display();
+   }
+
+   sorted = true;
+
+    
+}
+
+
+
+
+
+
+int main()
+{
+    
+    int iValue = 0;
+    
+    cout<<"Enter the number of elements : \n";
+    cin>>iValue;
+
+    ArrayX aobj(iValue);
+
+    aobj.Accept();
+
+    cout<<"Data before sorting\n";
+    aobj.Display();
+
+    aobj.BubbleSort();
+    cout<<"Data after sorting\n";
+    aobj.Display();
+
+
+
+    
+
+
+    
+    return 0;
+}
